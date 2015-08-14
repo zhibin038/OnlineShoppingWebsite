49ca242dadd6c5295839fac3f8ebe6c796a00c4d
zhibin038 <zhibin.zhang@outlook.com>
2015-08-14 00:49:37 -0700

    needCSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :carts
+  resources :carts
   get 'store/index'
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
