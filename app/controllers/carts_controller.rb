b35c3078543849a338b159f8980e6a63f9883e3b
zhibin038 <zhibin.zhang@outlook.com>
2015-08-16 17:41:07 -0700

    adding ajax

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
   # GET /carts.json
   def index
@@ -54,12 +54,21 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+   @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url,
+        notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
+
+  
+  def invalid_cart
+    logger.error "attempt to access invalid cart #{params[:id]}"
+    redirect_to store_url, noice: 'Invalid Cart'
+  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
-Rails.application.routes.draw do
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
+Buymore::Application.routes.draw do
+  resources :line_items
+
   resources :carts
-  resources :carts
-  get 'store/index'
+
+  get "store/index"
   resources :products
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'store#index', as: 'store'
+  # ...
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -43,7 +56,7 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-
+  
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
