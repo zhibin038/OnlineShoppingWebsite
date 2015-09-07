30605fbf75cd3525f396339f950c09789308d0ae
zhibin038 <zhibin.zhang@outlook.com>
2015-09-07 15:31:11 -0700

    fixbug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   include CurrentCart
   before_action :authorize, only:[:new]
   before_action :set_cart, only: [:new, :create, :index]
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_order, only: [:index, :edit, :update, :destroy]
 
   # GET /orders
   # GET /orders.json
